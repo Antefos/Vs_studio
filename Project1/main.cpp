05cde4a22d39e54b3a67cc0e22d5482e96642bc4
Antefos <ignoto.sm@gmail.com>
2022-02-02 13:11:52 +0100

    Vs_Code_Project
    Some trial

---
+++ Project1/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num{100};
+
+    cout << "Hello from Project1" << endl;
+    cout << "Enter your favorite number: ";
+    cin >> num;
+
+    cout << "Your favorite number is " << num << endl;
+    return 0;
+}
